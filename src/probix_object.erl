75fdc7c460ff907d9ffa4c2e01e7fd7d823b109c
glorybox <glorybox@localhost.localdomain>
2009-02-25 00:03:36 +0200

    CRUD functions in probix_object

--- src/probix_object.erl
+++ src/probix_object.erl
@@ -23,71 +23,39 @@ record_name() ->
 record_fields() ->
 	record_info(fields,object).
 
-read_all() ->
-	probix_db:read_all(object).
-
-%% read_all_json() ->
-%%	Objects = read_all(),
-%%	Json_objects = lists:map(
-%%		fun (Object) ->
-%%			utils:record_to_json_object(
-%%				record_info(fields, object),
-%%				Object
-%%			)
-%%		end,
-%%		Objects
-%%	),
-%%	mochijson2:encode(Json_objects).
-
 create(R) when is_record(R, object) ->
 	Id = probix_db:new_id(object),
 	Object = R#object{ id = Id },
 	{atomic, ok} = probix_db:write(Object),
 	Object.
 
-%% create_json(Json) ->
-%%	In = mochijson2:decode(Json),
-%%	Fields = record_info(fields, object),
-%%	R = utils:json_object_to_record(object, Fields, In),
-%%	Object = create(R),
-%%	Out = utils:record_to_json_object(Fields, Object),
-%%	mochijson2:encode(Out).
-
+create_from_json(Json) ->
+	R = probix_utils:json_to_record(Json, ?MODULE),
+	Object = create(R),
+	probix_utils:record_to_json(Object, ?MODULE).
+	
 read(Id) when is_integer(Id) ->
 	case probix_db:read({object, Id}) of
 		[ Object ] ->
 			Object;
 		[] ->
-			throw("object not found: " ++ integer_to_list(Id))
-	end.
+			throw({not_found, Id})
+	
+read_as_json(Id) when is_integer(Id) ->
+	Object = read(Id),
+	probix_utils:record_to_json(Object, ?MODULE).
 
-%% read_json(I) when is_list(I) ->
-%%	{Id, _} = string:to_integer(I),
-%%	Object = read(Id),
-%%	Out = utils:record_to_json_object(
-%%		record_info(fields, object),
-%%		Object
-%%	),
-%%	mochijson2:encode(Out).
-
-update(R) when is_record(R, object) ->
+update(Id, R) when is_record(R,object),is_integer(Id) ->
+	R#object{id = Id},
 	{atomic, ok} = probix_db:write(R),
 	R.
 
-%% update_json(Json) ->
-%%	In = mochijson2:decode(Json),
-%%	Fields = record_info(fields, object),
-%%	R = utils:json_object_to_record(object, Fields, In),
-%%	Object = update(R),
-%%	Out = utils:record_to_json_object(Fields, Object),
-%%	mochijson2:encode(Out).
+update_from_json(Id, Json) when is_list(Json),is_integer(Id) ->
+	Object = probix_utils:json_to_record(Json,?MODULE),
+	update(Id, Object).
 
 delete(Id) when is_integer(Id) ->
 	{atomic, ok} = probix_db:delete({object, Id}),
 	Id.
 
-%% delete_json(I) when is_list(I) ->
-%%	{I_int, _} = string:to_integer(I),
-%%	Id = delete(I_int),
-%%	mochijson2:encode({struct, [{id, Id}]}).
 
