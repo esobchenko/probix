e05492fb91785ebccde964c49dbb008ca39f1f08
Eugen Sobchenko <eugen@sobchenko.com>
2009-03-19 20:43:05 +0200

    probix_utils tests are now independent from probix_object module.

--- t/probix_utils_tests.erl
+++ t/probix_utils_tests.erl
@@ -1,6 +1,26 @@
 -module(probix_utils_tests).
+-compile(export_all).
 
 -include_lib("eunit/include/eunit.hrl").
+
+-record(foo, {foo, bar}).
+
+acceptable_value(none) ->
+	true;
+acceptable_value({_K, V}) when is_binary(V); is_integer(V) ->
+	true;
+acceptable_value(_Pair) ->
+	false.
+
+required_fields() ->
+	[foo].
+
+record_name() ->
+	foo.
+
+record_fields() ->
+	record_info(fields, foo).
+
 
 atom_to_binary_test_() ->
 	[
@@ -10,32 +30,32 @@ atom_to_binary_test_() ->
 json_object_to_record_test_() ->
 	[
 		?_assertMatch(
-			{object,1,2,3},
-			probix_utils:json_object_to_record({struct,[{<<"id">>,1},{<<"name">>,2},{<<"info">>,3}]}, probix_object)
+			{foo,1,2},
+			probix_utils:json_object_to_record({struct,[{<<"foo">>,1},{<<"bar">>,2}]}, ?MODULE)
 		),
 		?_assertThrow(
-			{missing_params, [name]},
-			probix_utils:json_object_to_record({struct,[{<<"info">>,3}]}, probix_object)
+			{missing_params, [foo]},
+			probix_utils:json_object_to_record({struct,[{<<"bar">>,3}]}, ?MODULE)
 		),
 		?_assertThrow(
-			{bad_values, [name]},
-			probix_utils:json_object_to_record({struct,[{<<"name">>,{struct, [<<"foo">>,1]}}]}, probix_object)
+			{bad_values, [foo]},
+			probix_utils:json_object_to_record({struct,[{<<"foo">>,{struct,[<<"baz">>,1]}}]}, ?MODULE)
 		)
 	].
 
 record_to_json_object_test_() ->
 	[
 		?_assertMatch(
-			{struct,[{<<"id">>,1},{<<"name">>,2},{<<"info">>,3}]},
-			probix_utils:record_to_json_object({object,1,2,3}, probix_object)
+			{struct,[{<<"foo">>,1},{<<"bar">>,2}]},
+			probix_utils:record_to_json_object({foo,1,2}, ?MODULE)
 		),
 		?_assertError(
-			{invalid_record, {foo,1,2,3}},
-			probix_utils:record_to_json_object({foo,1,2,3}, probix_object)
+			{invalid_record, {baz,1,2,3}},
+			probix_utils:record_to_json_object({baz,1,2,3}, ?MODULE)
 		),
 		?_assertError(
-			{invalid_record, {object,1,2}},
-			probix_utils:record_to_json_object({object,1,2}, probix_object)
+			{invalid_record, {foo,1}},
+			probix_utils:record_to_json_object({foo,1}, ?MODULE)
 		)
 	].
 
