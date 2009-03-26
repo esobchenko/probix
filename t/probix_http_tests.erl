c9199719525c0e6ff48e449d791a5cd8cd2685b7
Eugen Sobchenko <eugen@sobchenko.com>
2009-03-26 20:52:23 +0200

    rest_req/2, rest_req/3 helper functions added to probix_http_tests.erl

--- t/probix_http_tests.erl
+++ t/probix_http_tests.erl
@@ -5,8 +5,28 @@
 
 -include("probix.hrl").
 
+-define(URL, "http://127.0.0.1:8000").
+
 -define(O1, #object{id = 1, name = <<"foo">>, info = <<"bar">>}).
 -define(J1, probix_utils:record_to_json(?P1, probix_object)).
+
+rest_req('GET', Path) ->
+	Result = http:request(get, {?URL ++ Path, []}, [], []),
+	{ok, {{"HTTP/1.1", Status, _Reason}, _Headers, Body}} = Result,
+	{Status, Body};
+rest_req('DELETE', Path) ->
+	Result = http:request(delete, {?URL ++ Path, []}, [], []),
+	{ok, {{"HTTP/1.1", Status, _Reason}, _Headers, Body}} = Result,
+	{Status, Body}.
+
+rest_req('POST', Path, Data) ->
+	Result = http:request(post, {?URL ++ Path, [], [], Data}, [], []),
+	{ok, {{"HTTP/1.1", Status, _Reason}, _Headers, Body}} = Result,
+	{Status, Body};
+rest_req('PUT', Path, Data) ->
+	Result = http:request(put, {?URL ++ Path, [], [], Data}, [], []),
+	{ok, {{"HTTP/1.1", Status, _Reason}, _Headers, Body}} = Result,
+	{Status, Body}.
 
 basic_object_crud_test_() ->
 	{
@@ -27,8 +47,8 @@ basic_object_crud_test_() ->
 generate_basic_object_crud_tests(_) ->
 	[
 		?_assertMatch(
-			{ok, { {"HTTP/1.1",200,"OK"}, _Headers, "[]" }},
-			http:request("http://localhost:8000/objects")
+			{200, "[]"},
+			rest_req('GET', "/objects")
 		)
 	].
 
