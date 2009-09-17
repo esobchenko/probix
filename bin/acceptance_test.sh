65c84f5208def8a56db1dda569672a114a4615a4
Sergey Sinkovskiy <glorybox.away@gmail.com>
2009-09-17 14:07:14 +0300

    added running modes to startup scripts

--- bin/acceptance_test.sh
+++ bin/acceptance_test.sh
@@ -5,6 +5,10 @@ cd `dirname $0`/..
 export PROBIX_SERVER_IP="127.0.0.1"
 export PROBIX_SERVER_PORT="1234"
 
+## Setting running defaults
+export PROBIX_TEST_MODE=1
+export ERL_MAX_ETS_TABLES=100000
+
 erl \
 	-noshell \
 	-pa ebin \
