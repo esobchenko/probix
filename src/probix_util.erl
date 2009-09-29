6316fa24cc94c85a712b96debe06bab191be281c
Eugen Sobchenko <eugen@sobchenko.com>
2009-09-29 14:38:18 +0300

    probix_util.erl added

---
+++ src/probix_util.erl
@@ -0,0 +1,16 @@
+-module(probix_util).
+-export([random_string/1]).
+
+%% random strings are used for series ids
+random_string(Length) ->
+	{A1, A2, A3} = now(),
+	random:seed( A1, A2, A3 ),
+	lists:foldl( fun(_I, Acc) -> [do_rand(0) | Acc] end, [], lists:seq(1, Length) ).
+
+do_rand(R) when R > 46, R < 58; R > 64, R < 91; R > 96 ->
+	R;
+
+do_rand(_R) ->
+	do_rand(48 + random:uniform(74)).
+
+
