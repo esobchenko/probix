9840e59c455dbd6fcc86f5cd23001bde94c24dba
Eugen Sobchenko <eugen@sobchenko.com>
2009-09-29 17:08:05 +0300

    very_unique_id/0 added

--- src/probix_util.erl
+++ src/probix_util.erl
@@ -1,5 +1,5 @@
 -module(probix_util).
--export([random_string/1]).
+-export([random_string/1, very_unique_id/0]).
 
 %% random strings are used for series ids
 random_string(Length) ->
@@ -13,4 +13,5 @@ do_rand(R) when R > 46, R < 58; R > 64, R < 91; R > 96 ->
 do_rand(_R) ->
 	do_rand(48 + random:uniform(74)).
 
+very_unique_id() -> mochihex:to_hex(crypto:sha(term_to_binary({make_ref(), now()}))).
 
