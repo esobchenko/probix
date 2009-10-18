57fa4172fa6297d43eecf6e1459c4f96752ffb9c
Eugen Sobchenko <eugen@sobchenko.com>
2009-10-18 17:28:23 +0300

    parse_iso8601 added to probix_time.erl

--- src/probix_time.erl
+++ src/probix_time.erl
@@ -14,7 +14,55 @@
 
 %% probix_time:cmp(R1, R2). % returns 1 or 0 or -1
 
-parse_iso8601(_Time) -> ok.
+-record(timestamp, {year, month, day, hour, minute, second}).
+
+parse_iso8601(Time) when is_list(Time) ->
+	parse_iso8601( list_to_binary(Time) );
+
+parse_iso8601(Time) when is_binary(Time) ->
+	try
+		{ok, iso_to_rec(
+				year,
+				Time,
+				#timestamp{
+					year = 0,
+					day = 0,
+					month = 0,
+					hour = 0,
+					minute = 0,
+					second = 0
+				}
+			)
+		}
+	catch
+		error:_ -> {error, not_iso8601}
+	end.
+
+iso_to_rec( _State, <<>>, R ) -> R;
+
+iso_to_rec( State, <<"-", Rest/bitstring>>, R ) ->
+	iso_to_rec( State, Rest, R );
+iso_to_rec( State, <<" ", Rest/bitstring>>, R ) ->
+	iso_to_rec( State, Rest, R );
+iso_to_rec( State, <<"T", Rest/bitstring>>, R ) ->
+	iso_to_rec( State, Rest, R );
+iso_to_rec( State, <<":", Rest/bitstring>>, R ) ->
+	iso_to_rec( State, Rest, R );
+
+iso_to_rec( year, <<Year:32/bitstring, Rest/bitstring>>, R ) ->
+	iso_to_rec( month, Rest, R#timestamp{ year = Year } );
+iso_to_rec( month, <<Month:16/bitstring, Rest/bitstring>>, R ) ->
+	iso_to_rec( day, Rest, R#timestamp{ month = Month } );
+iso_to_rec( day, <<Day:16/bitstring, Rest/bitstring>>, R ) ->
+	iso_to_rec( hour, Rest, R#timestamp{ day = Day } );
+
+iso_to_rec( hour, <<Hour:16/bitstring, Rest/bitstring>>, R ) ->
+	iso_to_rec( minute, Rest, R#timestamp{ hour = Hour } );
+iso_to_rec( minute, <<Minute:16/bitstring, Rest/bitstring>>, R ) ->
+	iso_to_rec( second, Rest, R#timestamp{ minute = Minute } );
+iso_to_rec( second, <<Second:16/bitstring, Rest/bitstring>>, R ) ->
+	iso_to_rec( fraction, Rest, R#timestamp{ second = Second } ).
+
 
 parse_unix_epoch(_Time) -> ok.
 
