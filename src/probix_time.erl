bdd876e82de4e40859e1801ba6cb6e7a4e9b980d
Eugen Sobchenko <eugen@sobchenko.com>
2009-10-23 14:50:11 +0300

    minor changes in probix_time.erl; some tests added for probix_time:from_iso8601/1.

--- src/probix_time.erl
+++ src/probix_time.erl
@@ -1,117 +1,113 @@
 -module(probix_time).
 -compile(export_all).
 
-%% R1 = probix_time:parse_iso8601(Iso_8601_date).
-%% R2 = probix_time:parse_unix_epoch(Unix_epoch_date).
-
-%% {Date, Time} = probix_time:to_datetime(R1).
-%% Gregorian_seconds = probix_time:to_gregorian_seconds(R1).
-%% Unix_seconds = probix_time:to_unix_seconds(R1).
-
-%% String = probix_time:format("%Y-%M-%d %h:%m:%s", R1).
-
-%% R3 = probix_time:tz_convert(R1, Tz). % Tz = {-2, 0}
-
-%% probix_time:cmp(R1, R2). % returns 1 or 0 or -1
-
 -record(timestamp, {year, month, day, hour, minute, second, fraction, timezone}).
 -record(timezone, {hour, minute}).
 
 binary_to_integer(Binary) when is_binary(Binary) ->
-    list_to_integer(binary_to_list(Binary)).
+	list_to_integer(binary_to_list(Binary)).
 
-parse_iso8601(Time) when is_list(Time) ->
-	parse_iso8601( list_to_binary(Time) );
+%% We support a subset of ISO 8601: http://www.w3.org/TR/NOTE-datetime
 
-parse_iso8601(Time) when is_binary(Time) ->
-    try
-        {ok, iso_to_rec(
-            year,
-            Time,
-            #timestamp{
-                year = 0,
-                day = 0,
-                month = 0,
-                hour = 0,
-                minute = 0,
-                second = 0,
-                fraction = 0,
-                timezone = #timezone{ hour = 0, minute = 0}
-            }
-        )
-        }
-    catch
-        error:_ -> {error, not_iso8601}
-    end.
+from_iso8601(Time) when is_list(Time) ->
+	from_iso8601( list_to_binary(Time) );
 
-iso_to_rec( _State, <<>>, R ) -> R;
+from_iso8601(Time) when is_binary(Time) ->
+	try
+		{ok, parse_iso8601(
+			year,
+			Time,
+			#timestamp{
+				year = 0,
+				month = 0,
+				day = 0,
+				hour = 0,
+				minute = 0,
+				second = 0,
+				fraction = 0,
+				timezone = #timezone{ hour = 0, minute = 0}
+			})
+		}
+	catch
+		error:_ -> {error, bad_input}
+	end.
+
+parse_iso8601( _State, <<>>, R ) -> R;
 
 %% these are used to come from state of parsing time seconds or
 %% fraction seconds to parsing timezone
-iso_to_rec(second, <<"-", Rest/bitstring>>, R) ->
-    iso_to_rec(tz_hour_minus, Rest, R );
-iso_to_rec(fraction, <<"-", Rest/bitstring>>, R) ->
-    iso_to_rec(tz_hour_minus, Rest, R );
-iso_to_rec(_State, <<"+", Rest/bitstring>>, R) ->
-    iso_to_rec(tz_hour_plus, Rest, R );
-iso_to_rec(second, <<"Z", Rest/bitstring>>, R) ->
-    iso_to_rec(second, Rest, R);
-iso_to_rec(fraction, <<"Z", Rest/bitstring>>, R) ->
-    iso_to_rec(second, Rest, R);
+parse_iso8601(second, <<"-", Rest/bitstring>>, R) ->
+	parse_iso8601(tz_hour_minus, Rest, R );
+parse_iso8601(fraction, <<"-", Rest/bitstring>>, R) ->
+	parse_iso8601(tz_hour_minus, Rest, R );
+parse_iso8601(_State, <<"+", Rest/bitstring>>, R) ->
+	parse_iso8601(tz_hour_plus, Rest, R );
+parse_iso8601(second, <<"Z", Rest/bitstring>>, R) ->
+	parse_iso8601(second, Rest, R);
+parse_iso8601(fraction, <<"Z", Rest/bitstring>>, R) ->
+	parse_iso8601(second, Rest, R);
 
 %% separator between date
-iso_to_rec( State, <<"-", Rest/bitstring>>, R ) ->
-	iso_to_rec( State, Rest, R );
+parse_iso8601( State, <<"-", Rest/bitstring>>, R ) ->
+	parse_iso8601( State, Rest, R );
 
 %% separators between date and time
-iso_to_rec( separator, <<" ", Rest/bitstring>>, R ) ->
-	iso_to_rec( hour, Rest, R );
-iso_to_rec( separator, <<"T", Rest/bitstring>>, R ) ->
-	iso_to_rec( hour, Rest, R );
+parse_iso8601( separator, <<" ", Rest/bitstring>>, R ) ->
+	parse_iso8601( hour, Rest, R );
+parse_iso8601( separator, <<"T", Rest/bitstring>>, R ) ->
+	parse_iso8601( hour, Rest, R );
 
 %% separator between time parts
-iso_to_rec( State, <<":", Rest/bitstring>>, R ) ->
-	iso_to_rec( State, Rest, R );
+parse_iso8601( State, <<":", Rest/bitstring>>, R ) ->
+	parse_iso8601( State, Rest, R );
 
 %% separators of fractional part
-iso_to_rec( fraction, <<".", Rest/bitstring>>, R ) ->
-    iso_to_rec( fraction, Rest, R );
-iso_to_rec( fraction, <<",", Rest/bitstring>>, R ) ->
-    iso_to_rec( fraction, Rest, R );
+parse_iso8601( fraction, <<".", Rest/bitstring>>, R ) ->
+	parse_iso8601( fraction, Rest, R );
+parse_iso8601( fraction, <<",", Rest/bitstring>>, R ) ->
+	parse_iso8601( fraction, Rest, R );
 
 %% parsing date
-iso_to_rec( year, <<Year:32/bitstring, Rest/bitstring>>, R ) ->
-	iso_to_rec( month, Rest, R#timestamp{ year = binary_to_integer(Year) } );
-iso_to_rec( month, <<Month:16/bitstring, Rest/bitstring>>, R ) ->
-	iso_to_rec( day, Rest, R#timestamp{ month = binary_to_integer(Month) } );
-iso_to_rec( day, <<Day:16/bitstring, Rest/bitstring>>, R ) ->
-	iso_to_rec( separator, Rest, R#timestamp{ day = binary_to_integer(Day) } );
+parse_iso8601( year, <<Year:32/bitstring, Rest/bitstring>>, R ) ->
+	parse_iso8601( month, Rest, R#timestamp{ year = binary_to_integer(Year) } );
+parse_iso8601( month, <<Month:16/bitstring, Rest/bitstring>>, R ) ->
+	parse_iso8601( day, Rest, R#timestamp{ month = binary_to_integer(Month) } );
+parse_iso8601( day, <<Day:16/bitstring, Rest/bitstring>>, R ) ->
+	parse_iso8601( separator, Rest, R#timestamp{ day = binary_to_integer(Day) } );
 
 %% parsing time
-iso_to_rec( hour, <<Hour:16/bitstring, Rest/bitstring>>, R ) ->
-	iso_to_rec( minute, Rest, R#timestamp{ hour = binary_to_integer(Hour) } );
-iso_to_rec( minute, <<Minute:16/bitstring, Rest/bitstring>>, R ) ->
-	iso_to_rec( second, Rest, R#timestamp{ minute = binary_to_integer(Minute) } );
-iso_to_rec( second, <<Second:16/bitstring, Rest/bitstring>>, R ) ->
-	iso_to_rec( fraction, Rest, R#timestamp{ second = binary_to_integer(Second) } );
+parse_iso8601( hour, <<Hour:16/bitstring, Rest/bitstring>>, R ) ->
+	parse_iso8601( minute, Rest, R#timestamp{ hour = binary_to_integer(Hour) } );
+parse_iso8601( minute, <<Minute:16/bitstring, Rest/bitstring>>, R ) ->
+	parse_iso8601( second, Rest, R#timestamp{ minute = binary_to_integer(Minute) } );
+parse_iso8601( second, <<Second:16/bitstring, Rest/bitstring>>, R ) ->
+	parse_iso8601( fraction, Rest, R#timestamp{ second = binary_to_integer(Second) } );
 
 %% parsing fraction by character, cause we don't know its length
-iso_to_rec( fraction, <<C:8/bitstring, Rest/bitstring>>, R) ->
-    iso_to_rec( fraction, Rest, R#timestamp{ fraction = R#timestamp.fraction * 10 + binary_to_integer(C)});
+parse_iso8601( fraction, <<C:8/bitstring, Rest/bitstring>>, R) ->
+	parse_iso8601( fraction, Rest, R#timestamp{ fraction = R#timestamp.fraction * 10 + binary_to_integer(C)});
 
 %% parsing timezone info
-iso_to_rec( tz_hour_plus, <<Tz_hour:16/bitstring, Rest/bitstring>>, R) ->
-    iso_to_rec(tz_minute, Rest, R#timestamp{ timezone = (R#timestamp.timezone)#timezone{ hour = binary_to_integer(Tz_hour) }} );
-iso_to_rec( tz_hour_minus, <<Tz_hour:16/bitstring, Rest/bitstring>>, R) ->
-    iso_to_rec(tz_minute, Rest, R#timestamp{ timezone = (R#timestamp.timezone)#timezone{ hour = - binary_to_integer(Tz_hour) }} );
-iso_to_rec( tz_minute, <<Tz_min:16/bitstring, Rest/bitstring>>, R) ->
-    iso_to_rec(minute, Rest, R#timestamp{ timezone = (R#timestamp.timezone)#timezone{ minute = binary_to_integer(Tz_min) }} ).
+parse_iso8601( tz_hour_plus, <<Tz_hour:16/bitstring, Rest/bitstring>>, R) ->
+	parse_iso8601(
+		tz_minute,
+		Rest,
+		R#timestamp{ timezone = (R#timestamp.timezone)#timezone{ hour = binary_to_integer(Tz_hour) }}
+	);
+parse_iso8601( tz_hour_minus, <<Tz_hour:16/bitstring, Rest/bitstring>>, R) ->
+	parse_iso8601(
+		tz_minute,
+		Rest,
+		R#timestamp{ timezone = (R#timestamp.timezone)#timezone{ hour = - binary_to_integer(Tz_hour) }}
+	);
+parse_iso8601( tz_minute, <<Tz_min:16/bitstring, Rest/bitstring>>, R) ->
+	parse_iso8601(
+		minute,
+		Rest,
+		R#timestamp{ timezone = (R#timestamp.timezone)#timezone{ minute = binary_to_integer(Tz_min) }}
+	).
 
-
-
-
-
-parse_unix_epoch(_Time) -> ok.
+from_unix_epoch(_Time) -> ok.
 
 to_datetime(_Time) -> ok.
 
@@ -119,7 +115,9 @@ to_gregorian_seconds(_Time) -> ok.
 
 to_unix_seconds(_Time) -> ok.
 
-tz_convert(_R, _Tz) -> ok.
+to_utc(R) -> to_tz(R, #timezone{ hour = 0, minute = 0 }).
+
+to_tz(_R, _Tz) -> ok.
 
 cmp(_R1, _R2) -> ok.
 
--- test/probix_time_tests.erl
+++ test/probix_time_tests.erl
@@ -4,7 +4,93 @@
 -include("probix.hrl").
 -include_lib("eunit/include/eunit.hrl").
 
+-record(timestamp, {year, month, day, hour, minute, second, fraction, timezone}).
+-record(timezone, {hour, minute}).
+
+t() ->
+	#timestamp{
+		year = 0,
+		month = 0,
+		day = 0,
+		hour = 0,
+		minute = 0,
+		second = 0,
+		fraction = 0,
+		timezone = #timezone{ hour = 0, minute = 0}
+	}.
+
+-define(CI1, "1997").
+-define(CR1, (t())#timestamp{ year=1997 }).
+-define(CI2, "1997-07").
+-define(CR2, (t())#timestamp{ year=1997, month=7 }).
+-define(CI3, "1997-07-16").
+-define(CR3, (t())#timestamp{ year=1997, month=7, day=16 }).
+-define(CI4, "1997-07-16T19:20+01:00").
+-define(CR4,
+		(t())#timestamp{
+			year=1997,
+			month=7,
+			day=16,
+			hour=19,
+			minute=20,
+			timezone= #timezone{ hour=1, minute=0 }
+		}
+).
+-define(CI5, "1997-07-16T19:20:30+01:00").
+-define(CR5,
+		(t())#timestamp{
+			year=1997,
+			month=7,
+			day=16,
+			hour=19,
+			minute=20,
+			second=30,
+			timezone= #timezone{ hour=1, minute=0 }
+		}
+).
+-define(CI6, "1997-07-16T19:20:30.45+01:00").
+-define(CR6,
+		(t())#timestamp{
+			year=1997,
+			month=7,
+			day=16,
+			hour=19,
+			minute=20,
+			second=30,
+			fraction=45,
+			timezone= #timezone{ hour=1, minute=0 }
+		}
+).
+
 time_transform_test_() ->
 	[
+		?_assertEqual(
+			{ok, ?CR1},
+			probix_time:from_iso8601(?CI1)
+		),
+		?_assertEqual(
+			{ok, ?CR2},
+			probix_time:from_iso8601(?CI2)
+		),
+		?_assertEqual(
+			{ok, ?CR3},
+			probix_time:from_iso8601(?CI3)
+		),
+		?_assertEqual(
+			{ok, ?CR4},
+			probix_time:from_iso8601(?CI4)
+		),
+		?_assertEqual(
+			{ok, ?CR5},
+			probix_time:from_iso8601(?CI5)
+		),
+		?_assertEqual(
+			{ok, ?CR6},
+			probix_time:from_iso8601(?CI6)
+		),
+		?_assertEqual(
+			{error, bad_input},
+			probix_time:from_iso8601("foobar")
+		)
 	].
 
