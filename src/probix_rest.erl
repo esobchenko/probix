51fda4c951de82011d0e858033135927c34cdf60
Sergey Sinkovskiy <glorybox.away@gmail.com>
2011-04-01 15:36:40 +0300

    factored out probix_http, created probix_web

--- src/probix_rest.erl
+++ src/probix_rest.erl
@@ -6,40 +6,13 @@
 
 start(Options) ->
     log4erl:info(http_logger, "~p", [Options]),
-	mochiweb_http:start([{name, ?MODULE}, {loop, fun dispatch_request/1} | Options]).
+	mochiweb_http:start([{name, ?MODULE}, {loop, {?MODULE, dispatch_request}} | Options]).
 
 stop() ->
 	mochiweb_http:stop(?MODULE).
 
 dispatch_request(Req) ->
-	%% http method
-	Method = Req:get(method),
-
-	%% uri path
-	Path = Req:get(path),
-
-	%% uri query string as proplist
-	Query = Req:parse_qs(),
-
-	%% post body
-	Post = Req:recv_body(),
-
-	%% split path string for handy request handling
-	Splitted = string:tokens(Path, "/"),
-
-	R = try
-        log4erl:info(rest_logger, "Request-> Method: ~p, Path: ~p, Query: ~p, Post: ~p, Splitted: ~p", [Method, Path, Query, Post, Splitted]),
-		handle(Method, Splitted, Query, Post)
-	catch
-		%% regular throw exceptions
-		throw:Error when is_atom(Error) -> error(Error);
-
-		%% erlang errors and other exceptions
-		error:Exception ->
-			log4erl:info(rest_logger, "exception caught: ~p", [Exception]),
-			error(internal_error)
-	end,
-	Req:respond(R).
+    probix_http:dispatch_request(Req, ?MODULE).
 
 %% create series
 handle('POST', ["series"], Args, Post) ->
@@ -55,17 +28,17 @@ handle('POST', ["series"], Args, Post) ->
 			Series = probix_series:new_series(Label),
 			log4erl:info(rest_logger, "adding ticks to series: ~s", [ proplists:get_value(id, Series) ]),
 			probix_series:add_ticks(proplists:get_value(id, Series), Ticks),
-			redirect(Hostname ++ "/series/" ++ proplists:get_value(id, Series), probix_format:series_to_json([Series]));
+			probix_http:redirect(Hostname ++ "/series/" ++ proplists:get_value(id, Series), probix_format:series_to_json([Series]));
 
 		%% adding series without data
 		{error, empty_json} ->
 			log4erl:info(rest_logger, "creating series"),
 			Series = probix_series:new_series(Label),
-			redirect(Hostname ++ "/series/" ++ proplists:get_value(id, Series), probix_format:series_to_json([Series]));
+			probix_http:redirect(Hostname ++ "/series/" ++ proplists:get_value(id, Series), probix_format:series_to_json([Series]));
 
 		%% wrong data, doing nothing
 		{error, Error} ->
-			error(Error)
+			probix_http:error(Error)
 	end;
 
 %% update series with data
@@ -79,14 +52,14 @@ handle('POST', ["series", Id], [], Post) ->
 		{error, Error} ->
 			throw(Error)
 	end,
-	ok();
+	probix_http:ok();
 
 %% get all existing series
 handle('GET', ["series"], [], undefined) ->
 	log4erl:info(rest_logger, "getting all series"),
 	Series = probix_series:all_series(),
 	Content = probix_format:series_to_json(Series),
-	ok(Content);
+	probix_http:ok(Content);
 
 handle('GET', ["series", Id], Args, undefined) ->
 	probix_series:series(Id) == {error, not_found} andalso throw(not_found),
@@ -120,7 +93,7 @@ handle('GET', ["series", Id], Args, undefined) ->
 
 	Ticks = probix_series:get_ticks(Id, Range),
 	Content = probix_format:ticks_to_json(Ticks),
-	ok(Content);
+	probix_http:ok(Content);
 
 
 %% removing data from series
@@ -156,50 +129,11 @@ handle('DELETE', ["series", Id], Args, undefined) ->
 	end,
 
 	probix_series:delete_ticks(Id, Range),
-	ok();
+	probix_http:ok();
 
 handle('OPTIONS', _Any, _Query, _Post) ->
-    ok("");
+    probix_http:ok("");
 
 handle(_, _, _, _) ->
-	error(bad_request).
+	probix_http:error(bad_request).
 
-%%
-%% ok and error functions help to construct mochiweb's http response tuples;
-%% they used in handle/5 functions.
-%%
-
-%% empty response
-ok() ->
-	{200, [{"Content-Type", "application/json"}], ""}.
-
-ok(Content) ->
-	{200, [ { "Access-Control-Allow-Origin", "*" },
-            { "Access-Control-Allow-Methods", "OPTIONS, GET, POST, PUT, DELETE" },
-            { "Access-Control-Allow-Headers", "Accept, X-Requested-With" },
-            {"Content-Type", "application/json"},
-            {"Cache-Control", "no-cache"}], Content }.
-
-error(Error) ->
-	log4erl:error(rest_logger, Error),
-	{http_code(Error), [], ""}.
-
-% redirect(Location) ->
-% {301, [{"Location", Location}], ""}.
-
-redirect(Location, Content) ->
-	{301, [{"Location", Location}], Content}.
-
-%% returns http numeric response code for
-%% given error according to specification
-http_code(Error) when is_atom(Error) ->
-	case Error of
-		not_found ->
-			404;
-		unknown_format ->
-			406;
-		internal_error ->
-			500;
-		_Other ->
-			400
-	end.
